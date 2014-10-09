6a5cd31ff05bab87ac5ebf014720c363c5b2eade
Steven Diamond <stevend2@stanford.edu>
2014-10-09 11:57:32 -0700

    added install script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='subgradpy',
+    version='0.1',
+    author='Jaehyun Park',
+    packages=['subgradpy',
+              'subgradpy.functions'],
+    package_dir={'subgradpy': 'subgradpy'},
+)
